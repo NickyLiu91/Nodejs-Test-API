32fd584c8101694eff2b45f4323c8387407f7c0d
NickyLiu91 <nickyliu91@gmail.com>
2019-08-07 20:34:50 -0400

    route to get single course added

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express();
 
 const port = process.env.PORT || 3000
 
+const heroes = [
+  {id: 1, name: "Superman"},
+  {id: 2, name: "Batman"},
+  {id: 3, name: "The Flash"}
+]
+
 app.listen(port, () => {
    console.log(`server on port ${port}`);
 });
@@ -13,7 +19,16 @@ app.get("/", (req, res) => {
 })
 
 app.get("/api/heroes", (req, res) => {
-  res.send(["Superman", "Batman", "The Flash"]);
+  res.send(heroes);
+})
+
+app.get("/api/heroes/:id", (req, res) => {
+  let hero = heroes.find(c => c.id === parseInt(req.params.id))
+  if (!hero) {
+    res.status(404).send('The hero with the given ID was not found')
+  } else {
+    res.send(hero)
+  }
 })
 
 app.post("/", (req, res) => {
