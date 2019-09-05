2aec765e7ffcc499af527278a35b3601d18dfc80
NickyLiu91 <nickyliu91@gmail.com>
2019-09-05 03:22:23 -0400

    deleted excess files

--- api/models/thread.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const threadSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  title: String,
-  text: String
-});
-
-module.exports = mongoose.model('Account', threadSchema);
